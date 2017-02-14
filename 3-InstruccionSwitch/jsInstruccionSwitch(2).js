7e06c67daa7ee9f0b0fd03f03caf9aea78d18f0c
DiegoEzegonzalez <diegotkd9@gmail.com>
2017-02-14 12:06:46 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -5,6 +5,40 @@ var mesDelAño = document.getElementById('mes').value;
 
 alert (mesDelAño);
 
+switch (mesDelAño)
+
+{
+case "Enero":
+
+case "Febrero":
+
+case "Marzo":
+
+case "Abril":
+
+case "Mayo":
+alert("Falta para el frio");
+break;
+
+case "Junio":
+
+case "Julio":
+
+case "Agosto":
+alert("Abrigate si hace frio");
+break;
+
+case "Septiembre":
+
+case "Octubre":
+
+case "Noviembre":
+
+case "Diciembre":
+alert("Ya pasamos el frio, ahora calor");
+break;
+
+}
 
 
 
