087c5aa9b833c35e3f4199b42be3fa09ea79d9f4
DiegoEzegonzalez <diegotkd9@gmail.com>
2017-02-14 12:06:17 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (1).js
+++ 2-InstruccionIf/jsInstruccionIF (1).js
@@ -2,6 +2,19 @@ function Mostrar()
 {
 //tomo la edad  
 
+var edad;
+
+edad=document.getElementById('edad').value;
+
+if(edad==15)
+
+	{
+		alert("La niña bonita")
+
+	}
+
+
+
 
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -2,5 +2,28 @@ function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
 	
+var notaramdom;
+
+notaramdom=Math.floor(Math.random() * 10)	+1;
+
+if (notaramdom > 8 )
+
+{
+	alert("Excelente");
+}
+
+if (notaramdom > 3 && notaramdom < 9)
+
+{
+	alert("Aprobo");
+}
+
+if(notaramdom < 4)
+
+{
+	alert("Vamos la proxima se puede");
+}
+
+
 
 }//FIN DE LA FUNCIÓN
