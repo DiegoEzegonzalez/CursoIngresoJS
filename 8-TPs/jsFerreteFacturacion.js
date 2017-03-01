afb526fbf9105108992add96bf7380bf6658c8d4
DiegoEzegonzalez <diegotkd9@gmail.com>
2017-03-01 00:54:00 -0300

    Add files via upload

--- 8-TPs/jsFerreteFacturacion.js
+++ 8-TPs/jsFerreteFacturacion.js
@@ -5,13 +5,73 @@ C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
 {
+	var precioUno;
+	var precioDos;
+	var precioTres;
+	var sumaTotal;
+
+	precioUno=document.getElementById('PrecioUno').value;
+	precioDos=document.getElementById('PrecioDos').value;
+	precioTres=document.getElementById('PrecioTres').value;
+
+	precioUno=parseInt (precioUno);
+	precioDos=parseInt (precioDos);
+	precioTres=parseInt (precioTres);
+
+	sumaTotal=precioUno + precioDos + precioTres;
+
+	alert (" La suma total de los precios es: " +sumaTotal);
+
 	
 }
+
 function Promedio () 
+
 {
+	var precioUno;
+	var precioDos;
+	var precioTres;
+	var sumaTotal
+	var promedioTotal;
+
+	precioUno=document.getElementById('PrecioUno').value;
+	precioDos=document.getElementById('PrecioDos').value;
+	precioTres=document.getElementById('PrecioTres').value;
+
+	precioUno=parseInt (precioUno);
+	precioDos=parseInt (precioDos);
+	precioTres=parseInt (precioTres);
+
+	sumaTotal=precioUno + precioDos + precioTres;
+	promedioTotal=sumaTotal /3;
+
+	alert (" El promedio total de los precios es: " +promedioTotal);
 	
 }
+
 function PrecioFinal () 
+
 {
+	var precioUno;
+	var precioDos;
+	var precioTres;
+	var sumaTotal;
+	var precioFinalUno;
+	var precioFinalDos;
+	var precioFinalTres;
+
+	precioUno=document.getElementById('PrecioUno').value;
+	precioDos=document.getElementById('PrecioDos').value;
+	precioTres=document.getElementById('PrecioTres').value;
+
+	precioUno=parseInt (precioUno);
+	precioDos=parseInt (precioDos);
+	precioTres=parseInt (precioTres);
+
+
+
+
+
+	alert (" La precio total con iva: " +precioFinalUno);
 	
 }
